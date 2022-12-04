13bdb42ccedf7006ae60f913697a6af7f56be9c0
yantar <shevchenko9542@gmail.com>
2022-12-04 13:22:33 +0600

    Deleted: unnecessery dependecies
    Added: Main Application class (AlgoMathApplication)
    Deleted: Unnecessery dependencies

---
+++ src/main/java/com/algomath/AlgoMathApplication.java
@@ -0,0 +1,13 @@
+package com.algomath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoMathApplication {
+
+
+    public static void main(String[] args){
+        SpringApplication.run(AlgoMathApplication.class, args);
+    }
+}

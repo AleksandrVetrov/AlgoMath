8f469df450806d6efee61449428e057cb6b03f18
yantar <shevchenko9542@gmail.com>
2022-12-05 21:54:22 +0600

    Added: Simple WebMVC configuration

--- src/main/java/com/algomath/AlgoMathApplication.java
+++ src/main/java/com/algomath/AlgoMathApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AlgoMathApplication {
 
-
     public static void main(String[] args){
         SpringApplication.run(AlgoMathApplication.class, args);
     }

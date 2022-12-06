d3f20bd811be2e06b11aa4e3de2cfe38507dc4d9
yantar <shevchenko9542@gmail.com>
2022-12-06 20:45:23 +0600

    Added: EmailHandling configuration

---
+++ src/main/java/com/algomath/config/EmailHandlingConfig.java
@@ -0,0 +1,39 @@
+package com.algomath.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class EmailHandlingConfig {
+
+    @Value ("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.protocol}")
+    private String protocol;
+
+    @Bean
+    public JavaMailSender getMailSender() {
+        JavaMailSenderImpl emailSender = new JavaMailSenderImpl();
+
+        emailSender.setHost(host);
+        emailSender.setUsername(username);
+        emailSender.setPassword(password);
+        emailSender.setPort(port);
+        emailSender.setProtocol(protocol);
+
+        return emailSender;
+    }
+}

3b15c342b1668e7400324bc1574afad04056265b
Vladimir <gresvladimir02@gmail.com>
2022-12-20 11:11:49 +0600

    ad logger to EntryPoint

--- src/main/java/com/boots/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/boots/jwt/AuthEntryPointJwt.java
@@ -2,6 +2,8 @@ package com.boots.jwt;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -17,11 +19,12 @@ import java.util.Map;
 @Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
-
+    private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
             throws IOException {
+        logger.error("Unauthorized error: {}", authException.getMessage());
 
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);

07f196d5eb57df601f0d5cac648b8a3e7827a3e5
Vladimir <gresvladimir02@gmail.com>
2022-12-20 10:58:30 +0600

    Add repository for refreshToken

---
+++ src/main/java/com/boots/repository/RefreshTokenRepository.java
@@ -0,0 +1,18 @@
+package com.boots.repository;
+
+import java.util.Optional;
+
+import com.boots.entity.RefreshToken;
+import com.boots.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+
+    @Modifying
+    int deleteByUser(User user);
+}

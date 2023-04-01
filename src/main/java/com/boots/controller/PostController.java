78addb3f3cd65e761888b00be96e5ed2c4f94bd3
PFC <gresvladimir02@gmail.com>
2023-04-01 19:06:45 +0600

    Добавил PostController

---
+++ src/main/java/com/boots/controller/PostController.java
@@ -0,0 +1,42 @@
+package com.boots.controller;
+
+import com.boots.entity.Post;
+import com.boots.payload.request.PostDTO;
+import com.boots.service.PostService;
+import io.minio.errors.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/api/posts")
+public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping("/upload")
+    public Post createPost(@RequestBody PostDTO postDTO, @RequestParam MultipartFile[] files, @RequestParam Long userId)
+            throws IOException, InvalidKeyException, InsufficientDataException,
+            InternalException, ErrorResponseException, XmlParserException, ServerException, NoSuchAlgorithmException, InvalidResponseException, io.minio.errors.InternalException {
+        return postService.createPost(postDTO, files, userId);
+    }
+
+    @GetMapping
+    public List<Post> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    @GetMapping("/{userId}")
+    public List<Post> getPostsByUserId(@PathVariable Long userId) {
+        return postService.getPostsByUserId(userId);
+    }
+}
--- src/main/java/com/boots/service/RefreshTokenService.java
+++ src/main/java/com/boots/service/RefreshTokenService.java
@@ -40,17 +40,16 @@ public class RefreshTokenService {
         return refreshToken;
     }
 
-    public RefreshToken verifyExpiration(RefreshToken token) {
+    public void verifyExpiration(RefreshToken token) {
         if (token.getExpiryDate().compareTo(Instant.now()) < 0) {
             refreshTokenRepository.delete(token);
             throw new TokenRefreshException(token.getToken(), "Refresh token was expired. Please make a new signin request");
         }
 
-        return token;
     }
 
     @Transactional
-    public int deleteByUserId(Long userId) {
-        return refreshTokenRepository.deleteByUser(userRepository.findById(userId).get());
+    public void deleteByUserId(Long userId) {
+        refreshTokenRepository.deleteByUser(userRepository.findById(userId).get());
     }
 }
